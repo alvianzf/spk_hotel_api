e20afae2d24a60570c4d3847b0452121345d95a8
Alvian Zachry <alvian.faturrahman@webimp.com.sg>
2019-09-14 12:43:12 +0700

    minor fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -17,16 +17,16 @@ router.post('/', function(req, res) {
     })
 })
 
-router.put('/:id', function(req, res) {
-    User.findOneAndUpdate({_id: req.params.id}, req.body, function(err) {
+router.put('/', function(req, res) {
+    User.findOneAndUpdate({_id: req.body.id}, req.body, function(err) {
         if (err) res.status(422).json({status: 422, message: err.message});
 
         return res.status(200).json({status: 200, message: 'Updated'});
     })
 })
 
-router.delete('/:id', function(req, res) {
-    User.findOneAndDelete({_id: req.params.id}, function(err) {
+router.delete('/', function(req, res) {
+    User.findOneAndDelete({_id: req.body.id}, function(err) {
         if (err) return res.status(422).json({status: 422, message: err.message});
 
         return res.status(200).json({status: 200, message: 'Deleted'});
